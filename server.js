400bfe36adc936ab5bd3f837af90824fb73e75d6
psangani77 <30659159+psangani77@users.noreply.github.com>
2017-08-09 19:46:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-    res.send('Artice one will be requested and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+
 });
 
 app.get('/ui/style.css', function (req, res) {
